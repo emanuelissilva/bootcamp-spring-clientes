c70af75c6d84b4c2fcab37baf13884217f17645f
Gustavo <gustavo.wu@mercadolivre.com>
2021-05-27 18:24:39 -0300

    Depende da interfacce em vez da impl

--- src/main/java/com/bootcamp/clientesLoja/controller/ClienteController.java
+++ src/main/java/com/bootcamp/clientesLoja/controller/ClienteController.java
@@ -4,6 +4,7 @@ import com.bootcamp.clientesLoja.controller.dtos.PedidoDTO;
 import com.bootcamp.clientesLoja.domain.Cliente;
 import com.bootcamp.clientesLoja.domain.Pedido;
 import com.bootcamp.clientesLoja.service.ClienteServiceImpl;
+import com.bootcamp.clientesLoja.service.ClienteServiceInterface;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,34 +12,34 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
-    private ClienteServiceImpl clienteServiceImpl;
-
-    public ClienteController(ClienteServiceImpl clienteServiceImpl) {
-        this.clienteServiceImpl = clienteServiceImpl;
+    private ClienteServiceInterface clienteService;
+        
+    public ClienteController(ClienteServiceInterface clienteService) {
+        this.clienteService = clienteService;
     }
 
     @PostMapping
     ResponseEntity insereCliente(@RequestBody Cliente cliente) {
 
-        return new ResponseEntity(clienteServiceImpl.salvaCliente(cliente), HttpStatus.OK);
+        return new ResponseEntity(clienteService.salvaCliente(cliente), HttpStatus.OK);
     }
 
     @GetMapping
     ResponseEntity getCliente(@RequestParam Integer id) {
-        Cliente cliente = clienteServiceImpl.getClienteById(id);
+
+        Cliente cliente = clienteService.getClienteById(id);
         return new ResponseEntity(cliente, HttpStatus.OK);
     }
 
     @PostMapping("/pedido")
     ResponseEntity inserePedidoPorClienteId (@RequestBody PedidoDTO pedidoDTO) {
 
-        return new ResponseEntity(clienteServiceImpl.inserePedido(pedidoDTO), HttpStatus.OK);
+        return new ResponseEntity(clienteService.inserePedido(pedidoDTO), HttpStatus.OK);
     }
 
     @GetMapping("/pedido")
     ResponseEntity getPedidosPorClienteId(@RequestParam Integer id) {
 
-        return new ResponseEntity(clienteServiceImpl.getPedidos(id), HttpStatus.OK);
+        return new ResponseEntity(clienteService.getPedidos(id), HttpStatus.OK);
     }
-
 }
